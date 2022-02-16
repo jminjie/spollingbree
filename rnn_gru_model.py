960b24073c3305a35e96c113bcbb3f704b94bcba
Jordan <jminjie@umich.edu>
2022-02-15 22:07:18 -0800

    Simplify model
    Lower the memory footprint since the bee keeps crashing on my server.

--- rnn_gru_model.py
+++ rnn_gru_model.py
@@ -7,14 +7,13 @@ class RnnGRUModel(tf.keras.Model):
         super().__init__(self)
         self.vocab_size = 29
         self.embedding_dim = 256
-        self.rnn_units = 1024
+        self.rnn_units = 300
 
         self.embedding = tf.keras.layers.Embedding(self.vocab_size, self.embedding_dim)
         self.gru = tf.keras.layers.GRU(self.rnn_units,
                 return_sequences=True,
                 return_state=True)
-        self.dense1 = tf.keras.layers.Dense(self.vocab_size)
-        self.dense2 = tf.keras.layers.Dense(self.vocab_size)
+        self.dense = tf.keras.layers.Dense(self.vocab_size)
 
     def call(self, inputs, states=None, return_state=False, training=False):
         x = inputs
@@ -22,8 +21,7 @@ class RnnGRUModel(tf.keras.Model):
         if states is None:
             states = self.gru.get_initial_state(x)
         x, states = self.gru(x, initial_state=states, training=training)
-        x = self.dense1(x, training=training)
-        x = self.dense2(x, training=training)
+        x = self.dense(x, training=training)
 
         if return_state:
             return x, states
